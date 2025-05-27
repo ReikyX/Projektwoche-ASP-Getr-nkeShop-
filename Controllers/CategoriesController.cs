7c0ed93fee25f568d98f866387d466f417256843
ReikyX <188848574+ReikyX@users.noreply.github.com>
2025-05-27 11:06:37 +0200

    Edit and Create is fixed

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -86,7 +86,7 @@ namespace DrinkShop.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CategoryName")] Category category)
         {
             if (id != category.Id)
             {
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace DrinkShop.Models;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace DrinkShop.Models;
 
 public class Category
 {
     public int Id { get; set; }
     public string CategoryName { get; set; }
 
+    [ValidateNever]
     public ICollection<Product> Products { get; set; }
 
 }
